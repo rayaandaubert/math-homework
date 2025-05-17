c63817f083cd820bcf89c54c998e1a2c3d0c9d21
rayaandaubert <rayaandaubert@gmail.com>
2025-05-17 04:11:55 +0000

    Add 37.js

---
+++ src/37.js
@@ -0,0 +1,16 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function sumOfDigits(n) {
+    let sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n = Math.floor(n / 10);
+    }
+    return sum;
+}
